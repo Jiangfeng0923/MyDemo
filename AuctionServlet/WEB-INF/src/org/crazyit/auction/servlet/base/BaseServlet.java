232f0977875f2868083f0a3598e8c2ee6679f537
Jiangfeng0923 <821781378@qq.com>
2016-01-26 16:46:38 +0800

    add auction servlet

---
+++ AuctionServlet/WEB-INF/src/org/crazyit/auction/servlet/base/BaseServlet.java
@@ -0,0 +1,36 @@
+package org.crazyit.auction.servlet.base;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.springframework.context.ApplicationContext;
+/**
+ * Description:
+ * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
+ * <br/>Copyright (C), 2001-2014, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author  Yeeku.H.Lee kongyeeku@163.com
+ * @version  1.0
+ */
+public class BaseServlet extends HttpServlet
+{
+	private ApplicationContext ctx;
+	public void init(ServletConfig config)
+		throws ServletException
+	{
+		super.init(config);
+		// 获取Web应用中的ApplicationContext实例
+		ctx = WebApplicationContextUtils
+			.getWebApplicationContext(getServletContext());
+	}
+	// 返回Web应用中的Spring容器
+	public ApplicationContext getCtx()
+	{
+		return this.ctx;
+	}
+}
